8b0fe3402c9269b3138517ac95f698bddffada50
Geraldo Andrade <geraldo@geraldoandrade.com>
2022-08-21 18:22:59 +0200

    refactor(core): refactor configuration

--- pkg/config/settings.go
+++ pkg/config/settings.go
@@ -6,23 +6,20 @@ import (
 	"github.com/kelseyhightower/envconfig"
 )
 
-// Settings represents app configuration
-type Settings struct {
-	Debug                bool   `envconfig:"APP_DEBUG" default:"false"`
-	Port                 string `envconfig:"APP_PORT" default:"8080"`
-	Timeout              uint16 `envconfig:"API_TIMEOUT" default:"600"`
-	Version              string `envconfig:"APP_VERSION" default:"dev"`
-	DatabaseDSN          string `envconfig:"DATABASE_DSN" default:""`
-	DatabaseType         string `envconfig:"DATABASE_TYPE" default:""`
-	EnableAdminInterface bool   `envconfig:"ENABLE_ADMIN_INTERFACE" default:""`
-	AdminInterfacePath   string `envconfig:"ADMIN_INTERFACE_PATH" default:"./ui"`
+var S *Setting
+
+// Setting represents app configuration
+type Setting struct {
+	Port         string `envconfig:"APP_PORT" default:"8080"`
+	Timeout      uint16 `envconfig:"API_TIMEOUT" default:"600"`
+	Version      string `envconfig:"APP_VERSION" default:"dev"`
+	DatabaseDSN  string `envconfig:"DATABASE_DSN" default:""`
+	DatabaseType string `envconfig:"DATABASE_TYPE" default:""`
 }
 
-// NewSettings Provides a new value of settings
-func NewSettings(prefix string) Settings {
-	s := Settings{}
-	if err := envconfig.Process(prefix, s); err != nil {
+func init() {
+	S = &Setting{}
+	if err := envconfig.Process("", S); err != nil {
 		log.Fatal(err)
 	}
-	return s
 }
