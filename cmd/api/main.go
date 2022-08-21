c5a7405a2d3566af79514f6405fa214d4df284c9
Geraldo Andrade <geraldo@geraldoandrade.com>
2022-08-21 18:25:44 +0200

    refactor(core): refactor entrypoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,46 +2,37 @@ package main
 
 import (
 	"fmt"
+	"github.com/hosting-engine/envoy-proxy-manager-api/pkg/utils"
+	"log"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/filesystem"
-	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/gofiber/fiber/v2/middleware/requestid"
 	"github.com/hosting-engine/envoy-proxy-manager-api/pkg/config"
-	"log"
-	"net/http"
 )
 
 func main() {
-	settings := config.NewSettings("")
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		ReadTimeout:           time.Duration(60) * time.Second,
+		WriteTimeout:          time.Duration(60) * time.Second,
+		DisableKeepalive:      true,
+		DisableStartupMessage: true,
+		CaseSensitive:         true,
+		Concurrency:           0,
+		AppName:               "Envoy Proxy Manager API",
+	})
 
 	app.Use(cors.New())
 	app.Use(requestid.New())
 	app.Use(recover.New())
-	app.Use(logger.New(logger.Config{
-		Format:     "${time} - ${status} - ${method} ${path} ${latency}\n",
-		TimeFormat: "2006-01-02T15:04:05-0700",
-		TimeZone:   "Europe/Berlin",
-	}))
-
-	if settings.EnableAdminInterface {
-		app.Use(filesystem.New(filesystem.Config{
-			Root:         http.Dir(settings.AdminInterfacePath),
-			Browse:       false,
-			Index:        "index.html",
-			NotFoundFile: "404.html",
-			MaxAge:       3600,
-		}))
-	}
-
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
+		return c.SendString(fmt.Sprintf("Envoy Proxy Manager API v%s", utils.GetVersion()))
 	})
 
-	if err := app.Listen(fmt.Sprintf(":%s", settings.Port)); err != nil {
+	if err := app.Listen(fmt.Sprintf(":%s", config.S.Port)); err != nil {
 		log.Fatal(fmt.Errorf("%w", err).Error())
 	}
 }
